1e067fb7b790bad25497e58157753358722cdf12
Amelia Edmondson-Stait <amelia.edmondson-stait@ed.ac.uk>
2024-12-24 10:09:20 +0000

    forgot about tie break

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,16 +4,7 @@ function play(clicked_id) {
     document.getElementById("yourChoice").innerHTML = "Your choice: " + clicked_id
 
     // Get the computer to choose one random value from the two remaining options
-    let computerOptions; // define this variable before the if else statement
-    // identify the options for each selection
-    if(clicked_id == 'scissors'){
-        computerOptions = ['rock', 'paper']
-    }else if(clicked_id == 'rock'){
-        computerOptions = ['scissors', 'paper']
-    }else if(clicked_id == 'paper'){
-        computerOptions = ['scissors', 'rock']
-    }
-
+    let computerOptions = ['scissors', 'rock', 'paper']
     // pick a random value from the choices and display it as the computer choice
     let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)]
     document.getElementById("computerChoice").innerHTML = "Computer choice: " + computerChoice
@@ -23,8 +14,10 @@ function play(clicked_id) {
         clicked_id == 'paper' && computerChoice == 'rock' ||
         clicked_id == 'rock' && computerChoice == 'scissors'){
             document.getElementById("result").innerHTML = "Result: You win!"
-        }else{
-            document.getElementById("result").innerHTML = "Result: You loose!"
-        }
+    }else if(clicked_id == computerChoice){
+        "Result: tie break!"
+    else{
+        document.getElementById("result").innerHTML = "Result: You loose!"
+    }
         
 }
